0ca91b49658fc2028cf217182c6c7bdaa13e868f
laacademia <martingaray_12@hotmail.com>
2018-06-27 15:01:52 -0300

    Segunda Etapa Unlu

--- metadatos/componentes/toba_item/dump_18000087.sql
+++ metadatos/componentes/toba_item/dump_18000087.sql
@@ -1,19 +1,19 @@
 ------------------------------------------------------------
---[3538]--  ABM Tipos Pago 
+--[18000087]--  Material Estudio 
 ------------------------------------------------------------
 
 ------------------------------------------------------------
 -- apex_item
 ------------------------------------------------------------
 
---- INICIO Grupo de desarrollo 0
+--- INICIO Grupo de desarrollo 18
 INSERT INTO apex_item (item_id, proyecto, item, padre_id, padre_proyecto, padre, carpeta, nivel_acceso, solicitud_tipo, pagina_tipo_proyecto, pagina_tipo, actividad_buffer_proyecto, actividad_buffer, actividad_patron_proyecto, actividad_patron, nombre, descripcion, punto_montaje, actividad_accion, menu, orden, solicitud_registrar, solicitud_obs_tipo_proyecto, solicitud_obs_tipo, solicitud_observacion, solicitud_registrar_cron, prueba_directorios, zona_proyecto, zona, zona_orden, zona_listar, imagen_recurso_origen, imagen, parametro_a, parametro_b, parametro_c, publico, redirecciona, usuario, exportable, creacion, retrasar_headers) VALUES (
 	NULL, --item_id
 	'escuela', --proyecto
-	'3538', --item
+	'18000087', --item
 	NULL, --padre_id
 	'escuela', --padre_proyecto
-	'3525', --padre
+	'18000080', --padre
 	'0', --carpeta
 	'0', --nivel_acceso
 	'web', --solicitud_tipo
@@ -23,7 +23,7 @@ INSERT INTO apex_item (item_id, proyecto, item, padre_id, padre_proyecto, padre,
 	NULL, --actividad_buffer
 	NULL, --actividad_patron_proyecto
 	NULL, --actividad_patron
-	'ABM Tipos Pago', --nombre
+	'Material Estudio', --nombre
 	NULL, --descripcion
 	'15', --punto_montaje
 	NULL, --actividad_accion
@@ -35,12 +35,12 @@ INSERT INTO apex_item (item_id, proyecto, item, padre_id, padre_proyecto, padre,
 	NULL, --solicitud_observacion
 	NULL, --solicitud_registrar_cron
 	NULL, --prueba_directorios
-	NULL, --zona_proyecto
-	NULL, --zona
-	NULL, --zona_orden
-	'0', --zona_listar
-	'apex', --imagen_recurso_origen
-	NULL, --imagen
+	'escuela', --zona_proyecto
+	'zona_curso', --zona
+	'5', --zona_orden
+	'1', --zona_listar
+	'proyecto', --imagen_recurso_origen
+	'book.png', --imagen
 	NULL, --parametro_a
 	NULL, --parametro_b
 	NULL, --parametro_c
@@ -48,10 +48,10 @@ INSERT INTO apex_item (item_id, proyecto, item, padre_id, padre_proyecto, padre,
 	NULL, --redirecciona
 	NULL, --usuario
 	'0', --exportable
-	'2018-06-24 13:28:39', --creacion
+	'2018-06-27 10:12:28', --creacion
 	'0'  --retrasar_headers
 );
---- FIN Grupo de desarrollo 0
+--- FIN Grupo de desarrollo 18
 
 ------------------------------------------------------------
 -- apex_item_objeto
@@ -59,8 +59,8 @@ INSERT INTO apex_item (item_id, proyecto, item, padre_id, padre_proyecto, padre,
 INSERT INTO apex_item_objeto (item_id, proyecto, item, objeto, orden, inicializar) VALUES (
 	NULL, --item_id
 	'escuela', --proyecto
-	'3538', --item
-	'2636', --objeto
+	'18000087', --item
+	'18000482', --objeto
 	'0', --orden
 	NULL  --inicializar
 );
--- metadatos/permisos/grupo_acceso__admin.sql
+++ metadatos/permisos/grupo_acceso__admin.sql
@@ -241,4 +241,28 @@ INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, i
 	NULL, --item_id
 	'18000086'  --item
 );
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'escuela', --proyecto
+	'admin', --usuario_grupo_acc
+	NULL, --item_id
+	'18000087'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'escuela', --proyecto
+	'admin', --usuario_grupo_acc
+	NULL, --item_id
+	'18000088'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'escuela', --proyecto
+	'admin', --usuario_grupo_acc
+	NULL, --item_id
+	'18000089'  --item
+);
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'escuela', --proyecto
+	'admin', --usuario_grupo_acc
+	NULL, --item_id
+	'18000091'  --item
+);
 --- FIN Grupo de desarrollo 18
