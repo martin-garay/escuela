c130649559864f192c30359bf927ae90837b47aa
laacademia <martingaray_12@hotmail.com>
2018-08-19 15:59:19 -0300

    Gestion de usuarios

---
+++ php/operaciones/gestion_usuarios/gestion_usuario.php
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+	<?php 
+	$vinculo = "vinculador.get_url('toba_usuarios','1000228','generar_html',null)";
+	$popup = "abrir_popup('general',{$vinculo},{'resizable':1, 'scrollbars' : '1','width':'650px','height':'600px',celda_memoria:'nueva'})";
+	echo $popup;
+	?>
+</script>
