141be21e232ecfd9730372478633f1ce7d9c2e45
Martin <martingaray12@gmail.com>
2019-04-12 17:24:25 -0300

    commit desde produccion para guardar permisos

--- metadatos/permisos/grupo_acceso__carga_clases.sql
+++ metadatos/permisos/grupo_acceso__carga_clases.sql
@@ -4,16 +4,16 @@
 ------------------------------------------------------------
 INSERT INTO apex_usuario_grupo_acc (proyecto, usuario_grupo_acc, nombre, nivel_acceso, descripcion, vencimiento, dias, hora_entrada, hora_salida, listar, permite_edicion, menu_usuario) VALUES (
 	'escuela', --proyecto
-	'profesor', --usuario_grupo_acc
-	'profesor', --nombre
+	'carga_clases', --usuario_grupo_acc
+	'carga_clases_basico', --nombre
 	NULL, --nivel_acceso
-	'profesor', --descripcion
+	'Cargar Clases Basico', --descripcion
 	NULL, --vencimiento
 	NULL, --dias
 	NULL, --hora_entrada
 	NULL, --hora_salida
 	NULL, --listar
-	'1', --permite_edicion
+	'0', --permite_edicion
 	NULL  --menu_usuario
 );
 
@@ -24,80 +24,47 @@ INSERT INTO apex_usuario_grupo_acc (proyecto, usuario_grupo_acc, nombre, nivel_a
 --- INICIO Grupo de desarrollo 0
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'escuela', --proyecto
-	'profesor', --usuario_grupo_acc
+	'carga_clases', --usuario_grupo_acc
 	NULL, --item_id
 	'1'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'escuela', --proyecto
-	'profesor', --usuario_grupo_acc
+	'carga_clases', --usuario_grupo_acc
 	NULL, --item_id
 	'2'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'escuela', --proyecto
-	'profesor', --usuario_grupo_acc
+	'carga_clases', --usuario_grupo_acc
 	NULL, --item_id
-	'3537'  --item
+	'3559'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'escuela', --proyecto
-	'profesor', --usuario_grupo_acc
+	'carga_clases', --usuario_grupo_acc
 	NULL, --item_id
-	'3540'  --item
+	'3563'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'escuela', --proyecto
-	'profesor', --usuario_grupo_acc
+	'carga_clases', --usuario_grupo_acc
 	NULL, --item_id
-	'3544'  --item
+	'3565'  --item
 );
 --- FIN Grupo de desarrollo 0
 
 --- INICIO Grupo de desarrollo 18
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'escuela', --proyecto
-	'profesor', --usuario_grupo_acc
+	'carga_clases', --usuario_grupo_acc
 	NULL, --item_id
 	'18000079'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'escuela', --proyecto
-	'profesor', --usuario_grupo_acc
+	'carga_clases', --usuario_grupo_acc
 	NULL, --item_id
-	'18000080'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'escuela', --proyecto
-	'profesor', --usuario_grupo_acc
-	NULL, --item_id
-	'18000083'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'escuela', --proyecto
-	'profesor', --usuario_grupo_acc
-	NULL, --item_id
-	'18000085'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'escuela', --proyecto
-	'profesor', --usuario_grupo_acc
-	NULL, --item_id
-	'18000086'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'escuela', --proyecto
-	'profesor', --usuario_grupo_acc
-	NULL, --item_id
-	'18000087'  --item
+	'18000117'  --item
 );
 --- FIN Grupo de desarrollo 18
-
-------------------------------------------------------------
--- apex_grupo_acc_restriccion_funcional
-------------------------------------------------------------
-INSERT INTO apex_grupo_acc_restriccion_funcional (proyecto, usuario_grupo_acc, restriccion_funcional) VALUES (
-	'escuela', --proyecto
-	'profesor', --usuario_grupo_acc
-	'17'  --restriccion_funcional
-);
--- metadatos/permisos/grupo_acceso__profesor_practicas.sql
+++ metadatos/permisos/grupo_acceso__profesor_practicas.sql
@@ -4,10 +4,10 @@
 ------------------------------------------------------------
 INSERT INTO apex_usuario_grupo_acc (proyecto, usuario_grupo_acc, nombre, nivel_acceso, descripcion, vencimiento, dias, hora_entrada, hora_salida, listar, permite_edicion, menu_usuario) VALUES (
 	'escuela', --proyecto
-	'alumno', --usuario_grupo_acc
-	'alumno', --nombre
+	'profesor_practicas', --usuario_grupo_acc
+	'Profesor Clases Practicas', --nombre
 	NULL, --nivel_acceso
-	'Perfil Alumno', --descripcion
+	'Permite la carga de clase practicas', --descripcion
 	NULL, --vencimiento
 	NULL, --dias
 	NULL, --hora_entrada
@@ -24,68 +24,35 @@ INSERT INTO apex_usuario_grupo_acc (proyecto, usuario_grupo_acc, nombre, nivel_a
 --- INICIO Grupo de desarrollo 0
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'escuela', --proyecto
-	'alumno', --usuario_grupo_acc
+	'profesor_practicas', --usuario_grupo_acc
 	NULL, --item_id
 	'1'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'escuela', --proyecto
-	'alumno', --usuario_grupo_acc
+	'profesor_practicas', --usuario_grupo_acc
 	NULL, --item_id
 	'2'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'escuela', --proyecto
-	'alumno', --usuario_grupo_acc
+	'profesor_practicas', --usuario_grupo_acc
 	NULL, --item_id
-	'3514'  --item
+	'3565'  --item
 );
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'escuela', --proyecto
-	'alumno', --usuario_grupo_acc
+	'profesor_practicas', --usuario_grupo_acc
 	NULL, --item_id
-	'3516'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'escuela', --proyecto
-	'alumno', --usuario_grupo_acc
-	NULL, --item_id
-	'3523'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'escuela', --proyecto
-	'alumno', --usuario_grupo_acc
-	NULL, --item_id
-	'3524'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'escuela', --proyecto
-	'alumno', --usuario_grupo_acc
-	NULL, --item_id
-	'3571'  --item
+	'3567'  --item
 );
 --- FIN Grupo de desarrollo 0
 
 --- INICIO Grupo de desarrollo 18
 INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
 	'escuela', --proyecto
-	'alumno', --usuario_grupo_acc
+	'profesor_practicas', --usuario_grupo_acc
 	NULL, --item_id
-	'18000088'  --item
-);
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'escuela', --proyecto
-	'alumno', --usuario_grupo_acc
-	NULL, --item_id
-	'18000089'  --item
+	'18000079'  --item
 );
 --- FIN Grupo de desarrollo 18
-
-------------------------------------------------------------
--- apex_grupo_acc_restriccion_funcional
-------------------------------------------------------------
-INSERT INTO apex_grupo_acc_restriccion_funcional (proyecto, usuario_grupo_acc, restriccion_funcional) VALUES (
-	'escuela', --proyecto
-	'alumno', --usuario_grupo_acc
-	'16'  --restriccion_funcional
-);
